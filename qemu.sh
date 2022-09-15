675d338b091f27fe857315fd51a41a5f1f06126e
J.B. Langston <jb.langston@gmail.com>
2022-09-14 21:47:13 -0500

    Fix ability CD-ROMS mounts and variable overrides in qemu.sh

--- qemu.sh
+++ qemu.sh
@@ -17,17 +17,21 @@ mkdir -p $QEMUDIR
 cd $QEMUDIR
 shift
 
-# Prepare to run
-if [[ -f $CONF/prep.sh ]]; then
-  source $CONF/prep.sh
-fi
-
 QEMU="qemu-system-i386"
 QEMU_MACHINE="type=isapc"
 QEMU_SMP="1"
 QEMU_RAM="16M"
-QEMU_NET="ne2k_isa"
+QEMU_NET_DEVICE="ne2k_isa"
+QEMU_INTERNET=""
+QEMU_RETRONET="
+  -netdev socket,id=retronet,connect=:1234
+  -device $QEMU_NET_DEVICE,netdev=retronet"
 QEMU_EXTRA=""
+
+# Prepare to run
+if [[ -f $CONF/prep.sh ]]; then
+  source $CONF/prep.sh
+fi
 
 # Add -drive parameters for each image provided
 QEMU_DRIVES=""
@@ -47,20 +51,22 @@ for DRIVE in fda fdb hda hdb hdc hdd; do
   if [[ -f $DRIVE.img ]]; then
     QEMU_DRIVES="$QEMU_DRIVES -drive if=$INTERFACE,index=$INDEX,format=$FORMAT,file=$DRIVE.img"
   elif [[ -f $DRIVE.iso ]]; then
-    QEMU_DRIVES="$QEMU_DRIVES -drive if=$INTERFACE,index=$INDEX,format=$FORMAT,file=$DRIVE.iso"
+    QEMU_DRIVES="$QEMU_DRIVES -drive if=$INTERFACE,index=$INDEX,format=$FORMAT,media=cdrom,file=$DRIVE.iso"
   elif [[ -d $DRIVE ]]; then
     QEMU_DRIVES="$QEMU_DRIVES -drive if=$INTERFACE,index=$INDEX,format=raw,file=fat:rw:$DRIVE"
   fi
 done
+
+set | grep QEMU_
 
 # Run QEMU
 $QEMU \
   -machine $QEMU_MACHINE \
   -smp $QEMU_SMP \
   -m $QEMU_RAM \
-  -netdev socket,id=retronet,connect=:1234 \
-  -device $QEMU_NET,netdev=retronet \
   -serial mon:stdio \
+  $QEMU_INTERNET \
+  $QEMU_RETRONET \
   $QEMU_DRIVES \
   $QEMU_EXTRA \
   $@
