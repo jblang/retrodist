0fb902a52d1c548a1d960191b6a70b31382fe5d0
J.B. Langston <jb.langston@gmail.com>
2026-07-02 23:05:17 -0500

    Split slackware tty setup scripts into functional blocks

---
+++ slackware/tty-setup.sh
@@ -0,0 +1,269 @@
+# shellcheck shell=bash
+#
+# Shared QMP driver for Slackware's early tty setup installer.
+#
+# This script covers the menu-driven installer flow used by Slackware 1.1.1
+# through at least 2.2. Version-specific script.sh files should source it,
+# override defaults below when needed, then call tty_setup.
+
+TARGET_DISK=/dev/hda
+SWAP_MB=64
+
+# QEMU exposes the qemu.d/fat directory here; Slackware mounts it at FAT_MOUNT.
+FAT_PARTITION=/dev/hdb1
+FAT_MOUNT=/retro
+
+# Directory containing packages relative to the qemu.d/fat directory
+PACKAGE_DIR=/packages
+
+
+# These disk sets (and possibly more) are available
+#       A   - Base Linux system
+#       AP  - Various applications that do not need X
+#       D   - Program Development (C, C++, Kernel source, Lisp, Perl, etc.)
+#       E   - GNU Emacs
+#       F   - FAQ lists
+#       IV  - Interviews: libraries, include files, Doc and Idraw apps for X
+#       N   - Networking (TCP/IP, UUCP, Mail)
+#       TCL - Tcl/Tk/TclX, Tcl language, and Tk toolkit for developing X apps
+#       OI  - ObjectBuilder for X
+#       OOP - Object Oriented Programming (GNU Smalltalk 1.1.1)
+#       X   - XFree-86 2.0 Base X Windows System
+#       XAP - X Windows Applications
+#       XD  - XFree-86 2.0 X Windows program/server development system
+#       XV  - XView 3.2 release 5. (OpenLook [virtual] Window Manager, apps)
+#       Y   - Games (that do not require X)
+# You may specify any combination of disk sets at the prompt which follows,
+# including sets from SLS. For example - to install the base system, base X
+# windows, and the Tcl toolkit, you would enter: A X TCL
+# Use spaces to seperate the set names. You may use upper or lower case.
+PACKAGE_SETS="A AP D E F IV N TCL OI OOP X XAP XD XV Y"
+
+# Network configuration:
+NET_HOSTNAME=darkstar
+NET_DOMAINNAME=retro.net
+NET_IPADDR=10.0.2.15
+NET_NETWORK=10.0.2.0
+NET_GATEWAY=10.0.2.2
+NET_NETMASK=255.255.255.0
+NET_BROADCAST=10.0.2.255
+NET_NAMESERVER=10.0.2.3
+
+# Select one of these timezones:
+# Australia/LHI Australia/NSW Australia/North Australia/Queensland
+# Australia/South Australia/Tasmania Australia/Victoria Australia/West
+# Australia/Yancowinna Brazil/Acre Brazil/DeNoronha Brazil/East Brazil/West
+# Canada/Atlantic Canada/Central Canada/East-Saskatche Canada/Eastern
+# Canada/Mountain Canada/Newfoundland Canada/Pacific Canada/Yukon
+# Chile/Continental Chile/EasterIsland CET Cuba EET Egypt Factory GB-Eire GMT
+# GMT+0 GMT+1 GMT+10 GMT+11 GMT+12 GMT+13 GMT+2 GMT+3 GMT+4 GMT+5 GMT+6 GMT+7
+# GMT+8 GMT+9 GMT-0 GMT-1 GMT-10 GMT-11 GMT-12 GMT-2 GMT-3 GMT-4 GMT-5 GMT-6 GMT-
+# GMT-8 GMT-9 GMT0 GMT1 GMT10 GMT11 GMT12 GMT13 GMT2 GMT3 GMT4 GMT5 GMT6 GMT7
+# GMT8 GMT9 Greenwich Hongkong Iceland Iran Israel Jamaica Japan Libya
+# Mexico/BajaNorte Mexico/BajaSur Mexico/General MET NZ Navajo PRC Poland ROC ROK
+# Singapore SystemV/AST4 SystemV/AST4ADT SystemV/CST6 SystemV/CST6CDT
+# SystemV/EST5 SystemV/EST5EDT SystemV/MST7 SystemV/MST7MDT
+# SystemV/PST8 SystemV/PST8PDT SystemV/YST9 SystemV/YST9YDT Turkey UCT UTC
+# Universal US/Alaska US/Aleutian US/Arizona US/Central US/East-Indiana
+# US/Eastern US/Hawaii US/Michigan US/Mountain US/Pacific US/Pacific-New US/Samoa
+# W-SU WET Zulu
+TIMEZONE=US/Central
+
+# Log in to the installer environment as root.
+tty_setup_login_as_root() {
+    script_wait_line "darkstar login:"
+    script_send_line root
+}
+
+# Run the staged partitioning helper for the target disk.
+tty_setup_partition_disk() {
+    script_wait_line "#"
+    script_send_line "mount -t msdos $FAT_PARTITION /mnt &&
+/mnt/autoinst.d/diskpart.sh $TARGET_DISK $SWAP_MB &&
+umount /mnt"
+}
+
+# Launch Slackware's tty setup program.
+tty_setup_start_setup() {
+    script_wait_line "#"
+    script_send_line "setup"
+}
+
+# Keep the default keyboard map.
+tty_setup_keep_default_keyboard() {
+    script_wait_line "Would you like to remap your keyboard?"
+    script_send_line "2" # no
+}
+
+# Enable and initialize the configured swap partition.
+tty_setup_enable_swap() {
+    script_wait_line "Do you wish to install this partition as your swapspace ([y]es, [n]o)?"
+    script_send_line "y"
+    script_wait_line "Do you want setup to use mkswap on your swap partitions ([y]es, [n]o)?"
+    script_send_line "y"
+}
+
+# Install from scratch and format the root partition as ext2.
+tty_setup_format_root_ext2() {
+    script_wait_line "Would you like to [a]dd more software, or [i]nstall from scratch?"
+    script_send_line "i"
+    script_wait_line "What filesystem do you have (or do you plan to use) on your root"
+    script_wait_line "partition (/dev/hda2 ), [e]xt2fs or [x]iafs?"
+    script_send_line "e"
+    script_wait_line "Enter [i] again to install from scratch, or [a] to add"
+    script_send_line "i"
+    script_wait_line "Would you like to format this partition ([y]es, [n]o, [c]heck sectors too)?"
+    script_send_line "y"
+}
+
+# Mount the FAT staging partition inside the installed system.
+tty_setup_mount_staging_partition() {
+    script_wait_line "Would you like to set up some of these partitions to be visible"
+    script_wait_line "from Linux ([y]es, [n]o)?"
+    script_send_line "y"
+    script_wait_line "Please enter the partition you would like to access from Linux, or"
+    script_wait_line "type <q> to quit adding new partitions:"
+    script_send_line "$FAT_PARTITION"
+    script_wait_line "Where would you like to mount $FAT_PARTITION?"
+    script_send_line "$FAT_MOUNT"
+    script_wait_line "Done adding partition $FAT_PARTITION."
+    script_send_line "q"
+}
+
+# Select the FAT partition as the Slackware package source.
+tty_setup_select_hard_drive_source() {
+    script_wait_line "1 -- Install from a hard drive partition."
+    script_send_line "1"
+    script_wait_line "Please enter the partition where the Slackware sources can be"
+    script_wait_line "found, or [p] to see a partition list:"
+    script_send_line "$FAT_PARTITION"
+    script_wait_line "What directory are the Slackware sources in?"
+    script_send_line "$PACKAGE_DIR"
+    script_wait_line "What type of filesystem does your Slackware source partition contain?"
+    script_send_line "1" # FAT
+}
+
+# Select the Slackware package sets to install.
+tty_setup_select_disk_sets() {
+    script_wait_line "Which disk sets do you want to install?"
+    script_send_line "$PACKAGE_SETS"
+    script_wait_line "Do you want to use PROMPT mode (y/n)?"
+    script_send_line "y" # note: this doesn't prompt; it uses tagfiles
+}
+
+# Skip creating an installer boot disk.
+tty_setup_skip_boot_disk() {
+    script_wait_line "It is recommended that you make a boot disk." 600
+    script_wait_line "Would you like to do this ([y]es, [n]o)?"
+    script_send_line "n"
+}
+
+# Skip modem configuration.
+tty_setup_skip_modem_setup() {
+    script_wait_line "Would you like to set up your modem ([y]es, [n]o)?"
+    script_send_line "n"
+}
+
+# Skip mouse configuration.
+tty_setup_skip_mouse_setup() {
+    script_wait_line "Would you like to set up your mouse ([y]es, [n]o)?"
+    script_send_line "n"
+}
+
+# Install LILO to the target disk boot sector.
+tty_setup_install_lilo_to_mbr() {
+    script_wait_line "LILO (Linux Loader) Installation:"
+    # LILO, the Linux Loader, allows you to boot Linux directly off your hard drive
+    # without using a boot floppy disk.
+    # 1. If you are using OS/2's Boot Manager, this choice will allow you to boot
+    #    Linux from the Boot Manager menu. If you have already added the Linux
+    #    partition to the Boot Manager menu, this choice will complete the Boot
+    #    Manager installation process.
+    # 2. If you are planning to run Linux as the only operating system on your
+    #    machine, use this option to boot directly from the boot sector of your
+    #    drive. By the way, LILO may be removed from your boot sector using MS-DOS
+    #    fdisk with the command: fdisk /mbr
+    # 3. If you're not sure, select 3 to skip LILO and use a boot floppy instead.
+    #    You can read more about how to configure LILO manually in /usr/doc/lilo.
+    # 4. Install LILO to a formatted floppy. This is a safe choice, and will boot
+    #    considerably faster than a normal boot disk.
+    script_wait_line "Which option would you like? (1/2/3/4):"
+    script_send_line "2"
+}
+
+# Configure TCP/IP networking with the selected NET_* values.
+tty_setup_configure_network() {
+    script_wait_line "Would you like to configure your network ([y]es, [n]o)?"
+    script_send_line "y"
+    script_wait_line "Enter hostname:"
+    script_send_line "$NET_HOSTNAME"
+    script_wait_line "Enter domain name for $NET_HOSTNAME:"
+    script_send_line "$NET_DOMAINNAME"
+    script_wait_line "Do you plan to ONLY use loopback ([y]es, [n]o)?"
+    script_send_line "n"
+    script_wait_line "Enter IP address for $NET_HOSTNAME (aaa.bbb.ccc.ddd):"
+    script_send_line "$NET_IPADDR"
+    script_wait_line "Enter network address (aaa.bbb.ccc.ddd):"
+    script_send_line "$NET_NETWORK"
+    script_wait_line "Enter gateway address (aaa.bbb.ccc.ddd):"
+    script_send_line "$NET_GATEWAY"
+    script_wait_line "Enter netmask (aaa.bbb.ccc.ddd):"
+    script_send_line "$NET_NETMASK"
+    script_wait_line "Enter broadcast address (aaa.bbb.ccc.ddd):"
+    script_send_line "$NET_BROADCAST"
+    script_wait_line "Name Server for domain $NET_DOMAINNAME (aaa.bbb.ccc.ddd):"
+    script_send_line "$NET_NAMESERVER"
+}
+
+# Skip loading the selection daemon from rc.local.
+tty_setup_skip_selection_daemon() {
+    script_wait_line 'Would you like to add "selection -t none &" to /etc/rc.d/rc.local so that'
+    script_wait_line "selection will load at boot time ([y]es, [n]o)?"
+    script_send_line "n"
+}
+
+# Configure the installed system timezone.
+tty_setup_configure_timezone() {
+    script_wait_line "Would you like to configure your timezone ([y]es, [n]o)?"
+    script_send_line "y"
+    script_wait_line "Select one of these timezones:"
+    script_wait_line "Timezone?"
+    script_send_line "$TIMEZONE"
+}
+
+# Reboot from the installed hard disk.
+tty_setup_reboot_to_installed_system() {
+    script_wait_line "You may now reboot your computer by pressing control+alt+delete."
+    script_set_boot c
+    script_press_key ctrl-alt-delete
+}
+
+# Run the staged first-boot autoconfiguration script.
+tty_setup_run_first_boot_autoconf() {
+    script_wait_line "$NET_HOSTNAME login:"
+    script_send_line root
+    script_wait_line "$NET_HOSTNAME:~#"
+    script_send_line "$FAT_MOUNT/autoinst.d/autoconf.sh"
+}
+
+# Drive the full tty setup install sequence.
+tty_setup() {
+    tty_setup_login_as_root
+    tty_setup_partition_disk
+    tty_setup_start_setup
+    tty_setup_keep_default_keyboard
+    tty_setup_enable_swap
+    tty_setup_format_root_ext2
+    tty_setup_mount_staging_partition
+    tty_setup_select_hard_drive_source
+    tty_setup_select_disk_sets
+    tty_setup_skip_boot_disk
+    tty_setup_skip_modem_setup
+    tty_setup_skip_mouse_setup
+    tty_setup_install_lilo_to_mbr
+    tty_setup_configure_network
+    tty_setup_skip_selection_daemon
+    tty_setup_configure_timezone
+    tty_setup_reboot_to_installed_system
+    tty_setup_run_first_boot_autoconf
+}
